9defc71a513607bb00523354d925bc0c6a4d16fc
Badr <alzeibad>
2020-04-14 20:02:33 +0200

    fixing bugs

--- src/submissions/Praktikum0/main-nasm.asm
+++ src/submissions/Praktikum0/main-nasm.asm
@@ -3,72 +3,47 @@ global mainWithWhileLoop ;find DivSum using while loop
 global mainWithtForLoop ;find DivSum using For loop
 
 ;****************************Part 1 **********************
+
 mainWithWhileLoop:
-	push ebp    ;push the basepointer to Stack 
-	mov	ebp, esp   ;set the Top as the Base of the stack
-    mov eax, [esp + 4]  ;giving the eax the value of the input (one Steps after the ebp)
+	mov eax, [esp + 4] ; get Input from Stack to eax
+	cmp eax, 2  ;input < 2 then output the Input itself 
+	jle exit_Program_One
+	jng findSumDiv_WithWhileLoop ; else start Prog1
 
-;+++++++++++++++++++++++++ Test Input ++++++++++++++++++++
-;Step 1) TEST THE INPUT
-    cmp eax,eax    ;test Input if its Signed/unsigned
-    jle is_negative  ; Input is 0 or lower
-    jge is_bigger_Than_One   ; check if input is 1
-
-;Step 1-1)is_negative::negative -> pos.
-is_negative:   ; if Input is negative multi by (-1) 
-    imul eax,-1
-    jmp is_bigger_Than_One
-
-;Step 1-2) is_bigger_Than_Two::POSTIVE Input BUT check if its grater than one or two
-is_bigger_Than_One:
-    cmp eax, 1 
-      jle Out_One  ; return eax if the Input =< 2
-      jge is_bigger_Than_Two  ; start the Operatione for Input > 2
-
-is_bigger_Than_Two:
-    cmp eax, 2 
-      jle Out_Two  ; return eax if the Input =< 2
-      jge findSumDiv  ; start the Operatione for Input > 2
-
-;+++++++++++++++++++++++++ Start ++++++++++++++++++++++
-
-;Step 2) GET THE DEVIDERS for Positive Input>2
-findSumDiv:  ; this will finde the sum of the Divs using while loop
-
-    ;Obada Code is Here 
-
-
-
-
-
-    mov eax, 1000
-    jmp exit
+    findSumDiv_WithWhileLoop: 
         
         
-; 3) save the Input in eax and return it 
-Out_One:
-    mov eax,1 
-    jmp exit
-Out_Two:
-    mov eax,2 
-    jmp exit
-exit: 
-;this will clean the Stack 
-	pop	ebp     ; push the base out 
-	ret
+        mov eax, 4711 ;For Test Only
+
+        ;Obada Code 
+
+
+
+
+        jmp exit_Program_One
+	exit_Program_One:
+		ret
+
+
+
 
 ;**************************** Part2 **********************
-
 mainWithtForLoop:
-	push ebp    ;push the basepointer to Stack 
-	mov	ebp, esp   ;set the Top as the Base of the stack
-    mov eax, [esp + 8]  ;giving the eax the value of the input (two Steps after the ebp and the retunValue on the Stack)
+	mov eax, [esp + 8] 
+	cmp eax, 2
+	jle exit_Program_Two
+	jng findSumDiv_WithForLoop 
+
+    findSumDiv_WithForLoop: 
+        
+        
 
 
-    ; code Brishna here 
+        ;Brishna Code 
 
 
 
 
-    jmp exit 
-
+        jmp exit_Program_Two
+	exit_Program_Two:
+		ret
--- src/submissions/Praktikum0/main.c
+++ src/submissions/Praktikum0/main.c
@@ -6,32 +6,30 @@ extern void mainWithtForLoop(int value, int* result);
 
 int main()
 {
-
-long long num, *adress; //def the var of the entered Num   x64 Bit 
-long long *numAddress= &num; //his address of the nummber 
-int numInteger = (int) num ; //the Integer Value of 
-int sumWithPointers,sumWithoutPointers; // take the value of the two func from assm and display them
+long long num; //def the num of the entered Num   x64 Bit 
+int sumWithPointers; // take the value of the two func from assm and display them
 int pointer=0; // pointer
 int *pointerAddress = &pointer; // pointer address
+int numInteger; //the Integer Value of Entered Nummber
 
 //Intro
 printf("***** Program: dividers Sum of a given Nummber ***** \n"); 
 
 //Input num
-printf("Enter the Nummber ");
-scanf("%llu",&num);
+printf("Enter the Nummber \n ");
+scanf("%llu", &num);
+numInteger = (int) num; 
 
 // giving the value of the two imported funcs from assm to the two vars
 sumWithPointers = mainWithWhileLoop(numInteger);
-mainWithtForLoop (num, pointerAddress);
 
 //Outtro the num 
 printf("***** For the Nummber %llu is ***** \n" , num);
-
 //output result of the two funcs
-printf("the Sum of the dividers without Pointers %d is \n", sumWithoutPointers);
+printf("the Sum of the dividers without Pointers %d is \n", sumWithPointers);
 
-printf("the Sum of the dividers with pointers %d is \n", sumWithPointers);
+mainWithtForLoop (num, pointerAddress);
+printf("the Sum of the dividers with pointers %u is \n", *pointerAddress);
 
     return 0;
 }
