3d58c00434716901ff5361d90b2613af5f3e8668
brishnafarahi <63558620+brishnafarahi@users.noreply.github.com>
2020-04-30 14:52:58 +0200

    Update CgaChar.cc

--- src/submissions/Praktikum1/vorlage1/src/device/CgaChar.cc
+++ src/submissions/Praktikum1/vorlage1/src/device/CgaChar.cc
@@ -14,25 +14,24 @@
 // setzen des Zeichens
 void CgaChar::setChar(char c)
 {
-	
+this->c=c;
 
 }
 
 // auslesen des Zeichens
 char CgaChar::getChar()
 {
-	
+	return c;
 }
 
 // setzen der Darstellungsattribure
 void CgaChar::setAttr(const CgaAttr& attr)
 {
-	
+	this->attr = attr;
 }
 
 // auslesen der Darstellungsattribute
 CgaAttr CgaChar::getAttr()
 {
-
+return attr;
 }
-
