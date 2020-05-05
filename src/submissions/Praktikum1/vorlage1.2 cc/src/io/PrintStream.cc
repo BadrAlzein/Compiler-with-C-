71455bf3352a50a9a0b828602b968cadc64b4224
brishna farahi <brishna.farahi@b-tu.de>
2020-05-05 15:06:16 +0200

    printsrteam docu

--- src/submissions/Praktikum1/vorlage1.2 cc/src/io/PrintStream.cc
+++ src/submissions/Praktikum1/vorlage1.2 cc/src/io/PrintStream.cc
@@ -2,54 +2,55 @@
 #include "io/OutputChannel.h"
 
 
-// explicit one argument in constructor
-// explicit get the one parameter 
-// and convert the argument to another higher object with a function 
 // initialisierungslisten because of explicit
-PrintStream::PrintStream(OutputChannel* chan): channel(*chan)
+PrintStream::PrintStream(OutputChannel *chan) : channel(*chan)
 {
-	// already defined in private 
-	// OutputChannel& channel;
-	// zeige mit *chan auf den Zeiger von Outputchannel 
-	// also die speicheradresse von einem Outputchannel 
-	// this->channel = chan;
+	// already defined in private
+	// OutputChannel& channel is already defines in .h class
+	//*chan declares a pointer to the outputchannel
+	
 }
 
-PrintStream::PrintStream(OutputChannel& chan): channel(chan)
+PrintStream::PrintStream(OutputChannel &chan) : channel(chan)
 {
-	// this->channel = &chan;
+	//&chan  declares a reference to output channel
+	//takes the address of chan and calls outputchannel with that
 }
 
 // Ausgabe eines mit einem NULL-Byte terminierten Strings
-void PrintStream::print(const char* str)
+//NULL-Byte terminierten Stringsalso called as c strings 
+// when we define an array of characters like char array[10];
+void PrintStream::print(const char *str)
 {
 	// str is the pointer on a char
 	// &str is the adress of the pointer of str
-	// virtual int write(const char* data, int size) = 0;
-	for (;*str !='\0';(char*)str++) {
-		// char value of pointer has always the size of 1 bytes so 1
-		// print only one char, this char which the pointer shows on
+	//casting the str to char*
+	//A "string" is really just an array of chars; a null-terminated string is
+	// one where a null character '\0' marks the end of the string (not necessarily the 
+	//end of the array). All strings in code (delimited by double quotes "")
+	// are automatically null-terminated by the compiler.
+	//So for example, "hi" is the same as {'h', 'i', '\0'}.
+	
+	for (; *str != '\0'; (char *)str++)
+	{
+	
+	//is str a null term character? no : then increament 
+	// if its a null term charc then terminate the functon
 		this->channel.write(*str);
-		// sets the char pointer str 1 byte above 
+		
 		//(char*) str++;
 	}
 }
-
+// just write the char on channel
+// like console.write() for example
 void PrintStream::print(char ch)
 {
-	// THIS IS THE WRONG ONE int CgaChannel::write(const char* data, int size) 
-	/*
-	this is in OutputChannel.h implemented
-	int write(char c)
-	{
-		return write(&c, sizeof(c));
-	}
-	*/
+	
 	this->channel.write(ch);
 }
 
 // Ausgabe eines Strings mit anschliessendem Zeilenvorschub
-void PrintStream::println(const char* str)
+void PrintStream::println(const char *str)
 {
 	// two chars so two bytes
 	this->print(str);
@@ -57,16 +58,18 @@ void PrintStream::println(const char* str)
 }
 
 // Zeilenvorschub
+// '\n' calling a function that expects a character 
+//insert new line
 void PrintStream::println()
 {
 	this->channel.write('\n');
 }
 
-
-enum Base {
-		BINARY = 2,
-		DECIMAL = 10,
-		HEX = 16
+enum Base
+{
+	BINARY = 2,
+	DECIMAL = 10,
+	HEX = 16
 };
 /**
 Zahlen zur Basis 2 sowie zur Basis 16 sollten mit einem entsprechenden Praefix ausgegeben
@@ -81,204 +84,127 @@ wandlung in das Zweierkomplement ist nicht nötig aber erlaubt.
 // 2. Die Umwandlung der Ziffern in darstellbare Zeichen
 void PrintStream::print(int x, int base)
 {
-	// virtual int write(const char* data, int size) = 0;
-	// print negative numbers with sign
-	// 0xbla for hexadecimal base 16
-	// 0bbla for binary base 2
-	// bla has to be transfered before writing
-	// signed hex and bin is not defined so - will be before 0x or 0b
-/*	if ((x < 0 && base == BINARY) || (x < 0 && base == HEX)) {
-		this->print("Negative binary or hex numbers not possible.");
-	} else if ((base != BINARY) || (base != DECIMAL) || (base != HEX)) {
-		this->print("Diese Basis ist nicht erlaubt!");
-	} else {
-		if (x < 0) {
-		    x = -1 * x;
-		    // does not work
-		    // this->print(" -");
-		    this->print('-');
-		}
-		if (base == BINARY) {
-			this->print("0b");
-		} else if (base == HEX) {
-			this->print("0x");
-		}
-		this->print((unsigned) x, base);
-	}*/
-	if (x < 0) {
-	    x*= (-1);
-	    print('-');
-	   print((unsigned) x, base);
-	} else {
-		this->print((unsigned) x, base);
+	//0b is for binary its a binary prefix
+	//0x is a prefix for HEX 
+	if (x < 0)
+	{
+		x *= (-1);
+		print('-');
+		print((unsigned)x, base);
 	}
-
-	
+	else
+	{
+		this->print((unsigned)x, base);
+	}
 }
 
 void PrintStream::print(unsigned x, int base)
 {
 	// maximal reserved number chars length of 32 chars
-	// for unsigned integer size of 32 numbers 
-	// 4 Bytes for u integer -> 4 * 8 bit = 32 
-
-    char output[32];
+	// for unsigned integer size of 32 numbers
+	// 4 Bytes for u integer -> 4 * 8 bit = 32
+    // int has 32 bit max so we take aray of size 32
+	char output[32];
 	int length = 0;
-	const char characters[] ={"0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F"};
-	if (base == BINARY) {
+	const char characters[] = {"0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F"};
+	if (base == BINARY)
+	{
 		this->print("0b");
 
-		while(x){
-			output[length]=characters[(x%base)];
-			x /=base;
+		while (x)
+		{
+			output[length] = characters[(x % base)];
+			//x=x/base;
+			x /= base;
 			length++;
 		}
-			while (length){
-				channel.write(output[length-1]);
-				length--;
-			}
-			
-			}
-			
-	 else if (base == HEX) {
+		while (length)
+		{
+			channel.write(output[length - 1]);
+			length--;
+		}
+	}
+
+	else if (base == HEX)
+	{
 		this->print("0x");
-		if(x==0){
+		if (x == 0)
+		{
 			channel.write('0');
 		}
-			while(x){
-			output[length]=characters[(x%base)];
-			x /=base;
+		while (x)
+		{
+			output[length] = characters[(x % base)];
+			x /= base;
 			length++;
 		}
-			while (length){
-				channel.write(output[length-1]);
-				length--;
-			}
-			
-		
-	} else if (base == DECIMAL) {
-			while(x){
-			output[length]=characters[(x%base)];
-			x /=base;
+		while (length)
+		{
+			channel.write(output[length - 1]);
+			length--;
+		}
+	}
+	else if (base == DECIMAL)
+	{
+		while (x)
+		{
+			output[length] = characters[(x % base)];
+			x /= base;
 			length++;
 		}
-			while (length){
-				channel.write(output[length-1]);
-				length--;
-			}
-
-		
-	} else {
-		print("Eine erlaubte Basis aussuchen");
+		while (length)
+		{
+			channel.write(output[length - 1]);
+			length--;
+		}
+	}
+	else
+	{
+		print("please enter a valid base");
 		return;
 	}
 
-	
 	// A = 10 , B = 11, C = 12 .....
-	// will never change the field that is why const 
+	// will never change the field that is why const
 	//const char characters[] = { "0123456789ABCDEF" };
 	// BINARY = 2,
 	// DECIMAL = 10,
 	// HEX = 16
-	// initializing dynamicLength and output array
-	// TODO example x = 25
+	// initializing Length and output array
+	//  example x = 29
 	// init array from 0 to length always!
-	// dowhile because for output[0] you need a value 
+	// dowhile because for output[0] you need a value
 	// inits with 0 IS VERY IMPORTANT
-	/*int dynamicLength = 0;
-	if (x == 0) {
-		this->channel.write('0');
-	}
-	while(x) {
-		// TODO 25 % 10 = 5
-		// characters[5] = 5
-		output[dynamicLength] = characters[(x % base)];
+	/*
+	  29 % 2 = 1 (LSB), 14 % 2 = 0;..... so character [1]
+	  29/2= 14,..
+	  29 % 10 = 9
+		// characters[9] = 9
+		output[Length] = characters[(x % base)];
 		// x  := x/base
 		// divides and if it is not decimal it will be rounded down
-		// x = 25 / 10 = 2,5 => 2 because unsigned int rounded down
+		// example x = 25 / 10 = 2,5 => 2 because unsigned int rounded down
 		x /= base;
-		dynamicLength += 1;
+		Length += 1;
 	}
 	// reversed because needed output increase from 0 to length
 	// COMPARISON WITH INT CAST IS VERY IMPORTANT
-	while(dynamicLength) {
-		/*
-		this is in OutputChannel.h implemented
-		int write(char c)
-		{
-			return write(&c, sizeof(c));
-		}
-		*/
-		//this->channel.write(output[dynamicLength - 1]);
-		//dynamicLength--;
+	
+	//this->channel.write(output[Length - 1]);
+	//Length--;
 	//}
 
-/*
-if (base == DECIMAL || base == BINARY || base == HEX)
-    {
-        int zahl[32]; //Ein int hat maximal 32 Stellen
-        if (base == BINARY)
-        {
-            print("0b");
-        }
-        else if (base == HEX)
-        {
-            print("0x");
-        }
+	/*
 
-        if (x == 0)
-        {
-            print('0');
-        }
-        else
-        {
-            int i = 31;
-            while (x != 0)
-            {
-                zahl[i] = (x % base) + 48; //ASCII beginnt mit 0 erst bei 48
-                x = x / base;
-                i--;
-            }
-            i++; //beim letzten Mal wurde eins zu weit zurück gegangen
-            for (; i < 32; i++)
-            {
-                switch (zahl[i] - 48)
-                {
-                case 10:
-                    print('a');
-                    break;
-                case 11:
-                    print('b');
-                    break;
-                case 12:
-                    print('c');
-                    break;
-                case 13:
-                    print('d');
-                    break;
-                case 14:
-                    print('e');
-                    break;
-                case 15:
-                    print('f');
-                    break;
-                default:
-                    this->channel.write((char)zahl[i]);
-                }
-            }
-        }
-    }
-    else
-    {
-        print("Das ist keine schoene Basis!");
-        return;
-    }
+
+   
 */
 }
 
+
 // Zeigertypen werden immer zur Basis 16 ausgegeben!
-void PrintStream::print(void* p)
+void PrintStream::print(void *p)
 {
-	// p is an address so we pass the pointer address 
-  	this->print((unsigned) p, HEX);
+	// p is an address so we pass the pointer address
+	this->print((unsigned)p, HEX);
 }
