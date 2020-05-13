d8599a85f08712f05df9c5c325c51a58f5492316
Badr <alzeibad>
2020-05-13 18:30:32 +0200

    almost finish with badr part

--- src/submissions/Praktikum2/vorgabe2/include/thread/ActivityScheduler.h
+++ src/submissions/Praktikum2/vorgabe2/include/thread/ActivityScheduler.h
@@ -4,34 +4,49 @@
 /*
  * ActivityScheduler: 	Diese Klasse implementiert die Zustandsverwaltung
  * 			f�r Activities
- *
+ * 
+Zum Schluss kommt der ActivityScheduler . h . Er verwaltet die Ausf�hrungszust�nde der Prozesse.
+Er sorgt daf�r, dass ein Prozess wieder in die Ready-Liste kommt, wenn es der Ausf�hrungszu-
+stand erlaubt. Er sorgt auch daf�r, dass wenn Prozesse beendet werden, eventuelle Wartebedin-
+gungen eingehalten werden. Au�erdem kann der ActivityScheduler . h Prozesse komplett aus dem
+Scheduling entfernen.
+Testet zum Schluss Euer System zun�chst mit dem in mainAct.cc vorgegebenen Programm und
+testet zus�tzlich mit einem eigenen Programm die Funktionalit�t von sleep , wakeup und das Ver-
+halten beim Beenden von Prozessen. Dies ist nat�rlich bei der Abgabe zu demonstrieren.
  */
-
 #include "lib/Queue.h"
 #include "thread/Activity.h"
 #include "thread/Dispatcher.h"
 #include "thread/Scheduler.h"
 
-class ActivityScheduler: public Dispatcher, public Scheduler {
+class ActivityScheduler : public Dispatcher, public Scheduler
+{
 public:
 	ActivityScheduler()
 	{
-         isWaiting = false;
+		isSchedulerWaiting = false;
 	}
 
 	/* Initialisieren der ersten Aktivit�t, des Schedulers
 	 * und des Dispatchers.
 	 * Wird nur einmal aufgerufen.
 	 */
-	void start(Activity* act)
+	void start(Activity *act)
 	{
-        /*
-         *
-         * 
+		//assigning Activity to the scheduler means that scheduler is not waiting anymore
+		isSchedulerWaiting = false;
+
+		/* 1. init the activity for Scheduler
+         * 2. init the first activity for Dispatcher
+		 * PS. act is not an obj but a pointer
          */
-        
-        isWaiting = false;
-        
+
+		//Scheduler
+		act->changeTo(Activity::RUNNING);
+
+		//Dispatcher
+		Coroutine *activity = (Coroutine*) act; 
+		init(activity);
 	}
 
 	/* Suspendieren des aktiven Prozesses
@@ -47,24 +62,25 @@ public:
 	 * ist dem naechsten lauffaehigen Prozess die CPU
 	 * zuzuteilen.
 	 */
-	void kill(Activity*);
+	void kill(Activity *);
 
 	/* Terminieren des aktiven Prozesses,
 	 * und Wechsel zum naechsten lauffaehigen Prozess
 	 */
 	void exit();
-	
 
+	Activity *getActivity();
+	
 protected:
 	/* Der aktive Prozess ist, sofern er sich nicht im Zustand
 	 * Blocked oder Zombie befindet, wieder auf die Ready-Liste
 	 * zu setzen. Danach ist "to" mittels dispatch die Kontrolle
 	 * zu �bergeben.
 	 */
-	virtual void activate(Schedulable* to);
+	virtual void activate(Schedulable *to);
 
 private:
-    bool isWaiting
+	bool isSchedulerWaiting; //will be used to test if the Schedular is free or not 
 };
 
 extern ActivityScheduler scheduler;
--- src/submissions/Praktikum2/vorgabe2/src/thread/ActivityScheduler.cc
+++ src/submissions/Praktikum2/vorgabe2/src/thread/ActivityScheduler.cc
@@ -1,8 +1,6 @@
 #include "thread/ActivityScheduler.h"
 #include "io/PrintStream.h"
 
-
-
 /* This class will implements all func from the class in thread/ActivityScheduler.h
  * exit()
  * activate()
@@ -12,27 +10,37 @@
  */
 
 
-
 /* Terminieren des aktiven Prozesses,
 * und Wechsel zum naechsten lauffaehigen Prozess
 */
 void ActivityScheduler::exit(){
-    
-    
-    
+	/** to exit 
+	 * 1. kill the current activity 
+	 * 2. reschedule (activate the first process from the list)
+	*/
+    kill((Activity *) active()); //active() will point to the current activity
+	reschedule(); //send the next process on the top of ready list to the scedular
     
 }
 
 /* Explizites Terminieren des angegebenen Prozesses
 	 * Der Prozesszustand ist korrekt zu setzen
-	 * und der Prozess aus der Ready-Liste des Schedulers
-	 * zu entfernen. Falls der aktive Prozess zerstoert wird,
+	 * und 
+	 * 1. der Prozess aus der Ready-Liste des Schedulers
+	 * zu entfernen. 
+	 * 2. Falls der aktive Prozess zerstoert wird,
 	 * ist dem naechsten lauffaehigen Prozess die CPU
 	 * zuzuteilen.
 */
-void ActivityScheduler::kill(){
-    
-    
+void ActivityScheduler::kill(Activity *process){
+    //change the mode to Process deleteing mode 
+	process->changeTo(Activity::ZOMBIE);
+
+    //remove the process from the ready list 
+	remove((Schedulable *)process);
+
+	//send the next process on the top of ready list to the scedular
+	reschedule(); 
     
 }
 /* Der aktive Prozess ist, sofern er sich nicht im Zustand
@@ -40,25 +48,34 @@ void ActivityScheduler::kill(){
 	 * zu setzen. Danach ist "to" mittels dispatch die Kontrolle
 	 * zu übergeben.
 */
-void ActivityScheduler::activate(){
+void ActivityScheduler::activate(Schedulable *to){
     
     
     
 }
 
 /* Suspendieren des aktiven Prozesses
-* Der korrekte Ausfuehrungszustand ist zu setzen
-* und danach der naechste lauffaehige Prozess zu aktivieren.
+* 1. Der korrekte Ausfuehrungszustand ist zu setzen
+* und danach 
+* 2.der naechste lauffaehige Prozess zu aktivieren.
 */
 void ActivityScheduler::suspend(){
-    
+	/** to make a the Scheduler suspend his current Process 
+	 * 1. get the current process and block it 
+	 * 2. reschedule (activate the first process from the Ready list)
+	 */
+
+	//block the current activity  (getActivity() will get the current Activity) 
+	getActivity()->changeTo(Activity::BLOCKED);
+	
+	//send the next process on the top of ready list to the scedular
+	reschedule(); 
+}
+
+/*this will return a pointer to the current process Activity from dispatcher*/
+Activity *ActivityScheduler::getActivity()
+{
+    return (Activity *)this->active();
     
 }
 
-
-
-
-void ActivityScheduler::getActivitySchudler(){
-    
-    
-}
