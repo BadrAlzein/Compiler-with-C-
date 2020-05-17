950d6af1aa5522011dee9b26303e1d2681fa39a2
brishna farahi <brishna.farahi@b-tu.de>
2020-05-17 09:44:29 +0200

    3.2  losung

---
+++ src/submissions/Praktikum2/3.1/hello.h
@@ -0,0 +1,9 @@
+class Hello {
+public:
+
+	Hello(const char* name);
+	~Hello();
+	void body();
+
+	const char* name;
+};
---
+++ src/submissions/Praktikum2/3.1/runMe.sh
@@ -0,0 +1,16 @@
+echo "\nDas Programm hier ist die Implementierung für Aufgabe 3.1."
+echo "Es kann folgendermaßen ausgeführt werden:\n"
+
+echo "    \033[1;32mg++ main.cpp hello.cpp"
+echo "    ./a.out\033[1;37m\n"
+
+echo "Die Ausgabe müsste folgendes ergeben: \n"
+
+echo "    main1;"
+echo "    Anton ctor"
+echo "    Berta ctor"
+echo "    main2;"
+echo "    Anton body"
+echo "    main3;"
+echo "    Berta dtor"
+echo "    Anton dtor\n"
