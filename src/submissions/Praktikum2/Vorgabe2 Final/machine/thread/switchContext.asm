f0e8b5561b6a6f1dd8dffa70ac408bede739d708
BadrAlzein <BadrAlzein>
2020-05-19 10:09:34 +0200

    new asm Code

--- src/submissions/Praktikum2/Vorgabe2 Final/machine/thread/switchContext.asm
+++ src/submissions/Praktikum2/Vorgabe2 Final/machine/thread/switchContext.asm
@@ -17,24 +17,34 @@ csym switchContext
 
 switchContext:
 
-	;Prepare Stack1
+	;Prepare switch
 	push ebp			;save basePointer
-	mov ebp, esp		;move the stackpointer to the basePinter (Calling-Conv.)	
-	push ebx			;save all there registers used in Coroutine
-	push esi			;save all there registers used in Coroutine
-	push edi			;save all there registers used in Coroutine
-
-	;save before switching Stacks
+	
+	mov ebp, esp		;move the stackpointer to the basePinter (Calling-Conv.)
+	push ebx			;save all there callee-saved registers used in Coroutine
+	push esi			;save all there callee-saved registers used in Coroutine
+	push edi			;save all there callee-saved registers used in Coroutine
+	
+	;save before switching Stacks (using two registers eax & edx)
 	mov eax, [ebp + 8]	;save the 'from' in eax
+    mov edx, [ebp + 12] ;save the to 'to' in edx
+    
+    ;switch the Stack
 	mov [eax], esp		;save the stackPointer 1 in Adress of 'from'
-
-	;switch the Stack
-	mov eax, [ebp + 12] ;save the to Adress in eax
-	mov esp, [eax]		;return the saved old stackpointer to esp from Stack2
-
-	;clean up 
-	pop edi				;return all there registers used in Coroutine
-	pop esi				;return all there registers used in Coroutine
-	pop ebx				;return all there registers used in Coroutine
+	mov esp, [edx]		;return the saved old stackpointer to esp from Stack2
+    
+    
+    ; alternative way (using only one register eax)
+    ;	mov eax, [ebp + 8]	;save the 'from' in eax
+    ;	mov [eax], esp		;save the stackPointer 1 in Adress of 'from'
+    ;   mov eax, [ebp + 12] ;save the to Adress in eax
+    ;	mov esp, [eax]		;return the saved old stackpointer to esp from Stack2
+    
+    
+	;clean up
+	pop edi				;load new callee-saved registers
+	pop esi				;load new callee-saved registers
+	pop ebx				;load new callee-saved registers
+	
 	pop ebp				;return basePointer
 	ret					;Ruecksprung zum Aufrufer
