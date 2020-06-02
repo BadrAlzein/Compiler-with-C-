75cd75197d6f78d98bf13773a8cd2c00f9c5a54d
root <root@badr-MacBookPro.fritz.box>
2020-06-02 16:08:46 +0200

    vorgabe3 final

---
+++ src/submissions/Praktikum3/Vorgabe3/src/device/Clock.cc
@@ -0,0 +1,112 @@
+#include "device/Clock.h"
+#include "interrupts/IntLock.h"
+#include "device/PIT.h"
+#include "interrupts/InterruptVector.h"
+#include "thread/ActivityScheduler.h"
+
+/**	Spaetere Initialisierung...
+	 *	Hier ist nur im Konstruktor dafuer zu sorgen,
+	 *	dass sich Gate korrekt initialisieren kann
+	 */
+
+/**
+	 * Zur Behandlung der Interrupts: Jeder Interrupt-Quelle ist eine Vektornummer zugeordnet. 
+	 * Diese Nummern sind in der vorgegebenen Enumeration InterruptVector . h deklariert. 
+	 * Die Clock-Klasse muss sich von der vorgegebenen Klasse Gate.h ableiten,
+	 *  dem Gate-Konstruktor die Vektornummer mitteilen und eine handle () Methode definieren, 
+	 * um Interrupts behandeln zu können. 
+	 * Gate sorgt dann automatisch dafür, dass ein Eintrag an 
+	 * der richtigen Stelle der Vektortabelle erfolgt.
+	 * Wenn alles korrekt ist, wird dann bei jedem Timer-Interrupt 
+	 * automatisch Clock:: handle () aufgerufen.
+	 * */
+// initialise the clock
+//programable interval time is deifned in PIT.cc
+// initilalise the clock with the gate cause the class clock inherits the gate
+//use PIT cause thats what the clock does, handels the interrupts created in PIT
+Clock::Clock() : Gate(Timer), PIT()
+{
+
+	ticken = 0;
+}
+
+/**	Initialisierung des "Ticks" der Uhr
+	 *	Standardmaessig setzen wir das
+	 *	Uhrenintervall auf 20 Millisekunden
+	 *	und wir bekommen damit 50 Interrupts pro Sekunde
+	 *
+	 *	Zum Testen koennt Ihr bei Bedarf einen hoeheren Wert einstellen
+	 *	Weitere Hinweise zur Implementierung siehe "windup"
+	 */
+Clock::Clock(int us) : Gate(Timer), PIT(us)
+{
+	windup(us);
+}
+
+/**	Initialisierung des "Ticks" der Uhr
+	 * 	Die Einheit sind Mikrosekunden.
+	 * 	Hier ist der PIT geeignet zu initialisieren
+	 * 	und der PIT beim PIC anzustellen.
+	 */
+void Clock::windup(int us)
+{
+	//interval length
+	ticken = 0;
+	pit.interval(us);
+	
+	// set the interrupts
+	//Programmable Interrupt Controller ( PIC.h )
+	//interuppt quellen sind auch in PIT und PIC.h difiniert
+	// betreffende nummer des timers
+	pic.enable(PIC::PIT);
+}
+
+/** 	Der Interrupt-Handler fuer die Uhr.
+	 *	Hier ist der Interrupt am PIC zu bestaetigen
+	 *	und die Systemzeit zu aktualisieren.
+	 *	Die Systemzeit wird in Uhrenticks gemessen,
+	 *	Ihr muesst hier also nur mitzaehlen,
+	 *	wie oft die Uhr getickt hat.
+	 *
+	 *	Ueberpruefe zunaechst die Funktion des Timers
+	 *	indem Du jede Sekunde eine Ausgabe an der Bildschirmstelle (0,0)
+	 *	in Form eines rotierenden Propellers machst, der sich pro Sekunde
+	 *	ein kleines Stueck weiterdreht, wie der Sekundenzeiger einer Uhr.
+	 *	Der Propeller besteht aus der Zeichenfolge '/', '-', '\\', '|',
+	 *	d.h. nach 4 Sekunden ist eine komplette Umdrehung geschafft.
+	 *
+	 *	Achtung: Kommentiert alle anderen Ausgaben Eures Testprogrammes
+	 *	aus, sonst seht Ihr nichts!
+	 *
+	 *	Wenn der Uhrentest erfolgreich verlaufen ist, kommentiert
+	 *	den Propeller wieder aus und ruft stattdessen
+	 *	die "checkSlice" Methode des Schedulers auf,
+	 *	um diesen ggf. zum praeemptiven Rescheduling zu veranlassen.
+	 */
+/*
+the gate object that is created for every interuppt will be added
+ to logice-interupt table automatically
+and after this happend the interuppts occured 
+on every gate will be transfered to the driver via (through) handle method
+	*/
+// if the ticks of the clock is equal to the qauntim reset the clock and reschedule it
+void Clock::handle()
+{
+	IntLock safe;
+	pic.ack();
+	//ticken = this->
+	//brauche ich checkslice methode aus scheduler
+	this->ticken = ticks() + 1;
+	if ((ticks() / getIntervalTime()) % 20 == 0)
+	{
+		scheduler.checkSlice();
+	}
+}
+
+/* 	Liefert die Systemzeit in Ticks zurueck
+	 *	Kann hier "inline" implementiert werden
+
+	 int ticks()
+{
+}
+	 */
---
+++ src/submissions/Praktikum3/Vorgabe3/src/device/PIT.cc
@@ -0,0 +1,62 @@
+#include "device/PIT.h"
+
+
+/** Default-Konstruktor. Das Interval wird spaeter
+	  * mit der interval-Methode initialisiert */
+PIT::PIT():control_port(CONTROL_PORT), data_port(DATA_PORT) {}
+
+/** Initialisiert den Timers, sodass alle "us" Mikrosekunden ein Interrupt
+ * ausgeloest wird */
+PIT::PIT(int us) :control_port(CONTROL_PORT), data_port(DATA_PORT) 
+{
+	//this->controlPort = CONTROL_PORT;
+	//this->dataPort = DATA_PORT;
+	interval(us); //set the interval of (us) 
+}
+
+/** Setzt das Timer-Intervall, sodass alle "us" Mikrosekunden ein Interrupt
+* ausgeloest wird.
+* 
+* BEACHTE: Hier muss die Hardware korrekt programmiert werden.
+* Naeheres steht in der Web-Dokumentation.
+
+(from Project Doko)
+ Zur Einstellung eines 16 Bit Zählerwertes sind damit drei out-Befehle notwendig.
+ Der erste zum Übergeben des Steuerwortes und zwei weitere mit dem nieder- und 
+ dem höherwertigen Byte des Zählerwertes.
+*/
+void PIT::interval(int us)
+{
+		/*
+	every Step is from startValue until 0 d.h. 838ns 
+	TODO: delay the interrupt in (us) amout of steps -> that means us * 838ns
+	*/
+	intervalTime = us * TIME_BASE;  
+	
+
+	/** Controling the Hardware*/
+
+	/*
+	 * PIT is 16 byte Counter but the ports are all 8 bytes -> use steuerwort 
+	 *  to every Steuerwort use : 
+	 * 1. Bit0 is 0 binary 
+	 * 2. Bit1-3: Modus 2. Dabei wird bei Erreichen des Wertes 0 ein kurzer Impuls auf OUTx 
+	 * 	  ausgegeben und der Zähler wird automatisch wieder mit dem Startwert initialisiert,
+	 * 	  worauf der Vorgang wieder von vorn losgeht. 
+	 * 3. Bit4-5: use 10/01 to register the first and secound 6 bytes into the PIT
+	 * 4. Bit6-7: use the Counter for Periodische Unterbrechung -> use counter0
+	 **/
+
+//(Pending) -> dataPort
+
+//intervalTime 16 -> (00000000L) (00000000H) 
+
+	//write the low bytes of intervalTime
+	control_port.write(0b00010100); //low ->Bit4-5 is 01 -> 00 (01) 010 0
+	data_port.write(((char)(intervalTime & 0b11111111))); 
+
+	//write the high bytes of intervalTime
+	control_port.write(0b00100100); //high ->Bit4-5 is 10 -> 00 (01) 010 0
+	data_port.write(((char)((8>>intervalTime) & 0b11111111))); 
+    
+}
