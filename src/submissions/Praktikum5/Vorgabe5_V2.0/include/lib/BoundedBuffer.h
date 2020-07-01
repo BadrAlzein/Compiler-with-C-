a066b48df339125e55ce1a40b8af003a5a0a55b7
alzeinQgmail.com <badr@gmail.com>
2020-07-01 14:56:31 +0200

    puffer

--- src/submissions/Praktikum5/Vorgabe5_V2.0/include/lib/BoundedBuffer.h
+++ src/submissions/Praktikum5/Vorgabe5_V2.0/include/lib/BoundedBuffer.h
@@ -1,12 +1,7 @@
 #ifndef BoundedBuffer_h
 #define BoundedBuffer_h
-
-#include "lib/Queue.h"
 #include "thread/ActivityScheduler.h"
 #include "interrupts/IntLock.h"
-#include "io/PrintStream.h"
-
-extern PrintStream out;
 
 /** Diese Klasse stellt einen begrenzten synchronisierten
  *  Puffer dar, welcher von Interruptbehandlungsroutinen
@@ -16,130 +11,83 @@ extern PrintStream out;
  *  erstmal nur, das ihr alle Methoden hier im Header implementieren
  *  m�sst.
  */
-template <typename T, unsigned size>
-class BoundedBuffer
-{
+template<typename T,unsigned size>
+class BoundedBuffer {
 public:
-	BoundedBuffer()
-	{
-		// amount of elements in buffer
-		sheeps = 0;
-		// pointer for buffer in get method
-		getPointer = 0;
-		// pointer for buffer in add method
-		addPointer = 0;
-		// one pointer on sleeper
-		sleeper = 0;
-	}
+	BoundedBuffer(){}
 
 	/** Diese Methode wird vom Interrupthandler aufgerufen, um
-	 *  ein etwas in den Puffer zu schreiben. Ist der Puffer voll,
-	 *  werden die zu schreibenden Daten verworfen.
-	 *  Prozesse die auf eine Eingabe warten m�ssen hier geweckt werden.
+	 *  ein etwas in den Puffer zu schreiben. 
+	 * //SECTION: buffer Voll 
+	 *  Ist der Puffer voll,werden die zu schreibenden Daten verworfen.
+	 * //SECTION: fair sync 
+	 * Prozesse die auf eine Eingabe warten m�ssen hier geweckt werden.
 	 */
-	void add(T &elem)
-	{
-		// test if the buffer is full => get method could decrease the sheeps so amount of elements in buffer
-		if (this->sheeps < (int) size)
-		{
-			// index 0 could be written because addPointer is 0 at beginning
-			this->buffer[addPointer] = elem;
-			this->addPointer = (this->addPointer + 1) % size;
-			// counter for the written codes
-			this->sheeps++;
-			// after we increased an added element then we could safely wake up another sleeper to read the element out of the buffer 
-			// if we got a sleeper then wake him up
-			if (this->sleeper != 0)
-			{
-				// this is the read process which was get in to sleep in get method: 
-				// this->sleeper = (Activity *) scheduler.active();
-				Activity *sleeper = this->sleeper;
-				// reset the sleeper 
-				this->sleeper = 0;
-				// Prozesse die auf eine Eingabe warten muessen hier geweckt werden.
-				sleeper->wakeup();
+	void add(T& elem)
+	{	
+		//if buffer not full
+		if(population < size){
+			population ++; //inc buffer counter
+			buffer[pushPos] = elem; //add the elemet to the head 
+			pushPos= (pushPos+1) % size; // inc the push Position (if pushPos=size -> 0) 
+
+			//SECTION: fair sync 
+			/** if there is any waiting Activity */
+			if (suspendedAct !=0){
+				Activity *wackedAct = suspendedAct;
+				wackedAct->wakeup();//wakeup this
+				suspendedAct=0;	//reset the  suspended Activity Trigger
 			}
-		}
-		/*
-		if(((int)((rear+1) % size) == front) && (rear != -1)){
-			//error OVERFLOW
-		} else {
-			//out.println("ich schreibe");
-			rear = (rear+1)%size;	
-			buffer[rear] = elem;
-			Activity* waking = (Activity*) sleepers.dequeue();
-			if (waking != 0) {
-				waking->wakeup();
-			}
-			//out.println();
-			//out.println("ich habe was geschrieben");
-		}
-		 */
+		}else {
+		//SECTION: buffer Voll 
+		//if the buffer full dont add anything
+		//TODO: (not wanted)  delete the oldest elem and add the new elem 
+ 		}
+	
 	}
 
 	/** Diese Methode wird von Prozessen aufgerufen, um Daten aus dem
-	 *  Puffer zu lesen. Ist dieser leer wird der lesende Prozess schlafen
-	 *  gelegt. Achtet hier besonders auf die Synchronisierung.
+	 *  Puffer zu lesen. 
+	 * //SECTION:empty buffer
+	 * Ist dieser leer wird der lesende Prozess schlafen gelegt.
+	 * //SECTION: sync
+	 * Achtet hier besonders auf die Synchronisierung.
 	 */
 	T get()
 	{
-		/* 
-		T temp;					//temporaere variable zur ausgabe
-		if (rear == -1) { 			//buffer ist leer
-			//out.println("erster fall");
-			Activity* active = (Activity*) scheduler.getActiveProcessActivity();
-			sleepers.enqueue(active);
-			active->sleep();
-				
-		} else if (front == rear) {		//letztes element		
-			temp = buffer[front];
-			front = 0;
-			rear = -1;
-		} else {				//normaler fall
-			temp = buffer[front];
-			front = (front+1)%size;
-			// rear = -1;
+		//SECTION: sync
+		//IntLock lock; //save this func from Synchronisierung mistakes
+		popPos = (popPos+1) % size; //get the index of the wanted elem (if popPos=size -> 0)
+		//SECTION:empty buffer
+		if(population==0){
+			suspendedAct = (Activity *) scheduler.active();
+			scheduler.suspend(); //suspend the current activity 
 		}
-		return temp;
-		*/
-		// get could be interrupted by another process 
-		// KernelLock lock;
-		// buffer is cleared if no element in buffer (sheep)
-		if (this->sheeps == 0)
-		{
-			// Ist dieser leer wird der lesende Prozess schlafen gelegt.
-			this->sleeper = (Activity *) scheduler.active();
-			// naechstes element in ready liste wird aktiv
-			scheduler.suspend();
-		}
-		// danach ist wieder ein element im buffer zu befuellen
-		this->sheeps--;
-		// take the elements out of the buffer so get the next index of buffer
-		this->getPointer = (this->getPointer + 1) % size;
-		if (this->getPointer != 0) {
-			// other elements before last element
-			return this->buffer[this->getPointer - 1];
-		} else {
-			// if the index is 0 the getPointer shows on first element in buffer but we want the last element
-			// because 
-			// return last element 
-			// this is neccessary because % modulo size resets the last element to 0 
-			int lastElement = size - 1; 
-			return this->buffer[lastElement];
+		population--; 
+
+		/* get the last elem from buffer */
+		if (popPos == 0) { //if the buffer voll 
+			return buffer[size-1]; // -1 because of % size will set the pop on 0
+		}else {
+			return buffer[popPos-1];
 		}
 	}
 
-	bool isCleared() 
+		bool isCleared() 
 	{
-		return this->sheeps == 0;
+		return population == 0;
 	}
-
+	
+	
+bool buffer_clear(){
+	return population==0;
+}
 private:
 	T buffer[size];
-	unsigned addPointer;
-	Activity *sleeper;
-	int sheeps;
-	unsigned getPointer;
+	unsigned population = 0 ; //the population of the buffer
+	unsigned pushPos = 0, popPos = 0 ;  //shows where to push/pop elements out of the buffer 
+	Activity *suspendedAct = 0; //the suspended Activity 
 };
 
 #endif
+
